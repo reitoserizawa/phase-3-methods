80f1e356f463b636fc5781d103789ea736c435dc
ReitoSerizawa <s.reitiger@gmail.com>
2022-05-16 16:38:40 -0400

    commit tree fdea53b56430848d7dad8817420fc34514e81933

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    return num1 + num2
+end
+
+def halve x
+    if x.class == Integer
+        return x / 2 
+      else
+        return nil
+    end
+end
